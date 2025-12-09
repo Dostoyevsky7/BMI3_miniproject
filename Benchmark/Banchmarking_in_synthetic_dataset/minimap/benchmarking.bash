861d2c28c58db7127a6720603f46ba2189e8212a
HUANG YUFEI <156776918+Dostoyevsky7@users.noreply.github.com>
2025-12-09 16:47:14 +0800

    Delete Benchmark/Banchmarking_in_synthetic_dataset directory

--- Benchmark/Banchmarking_in_synthetic_dataset/minimap/benchmarking.bash
+++
@@ -1,5 +0,0 @@
-python evaluate_repeats.py \
-    --truth ground_truth_repeats.tsv\
-    --pred syn_adjust.tsv\
-    --overlap 0.8\
-    --pred-format minimap
--- Benchmark/Banchmarking_in_synthetic_dataset/minimap/run_minimap_in_syn.bash
+++
@@ -1,4 +0,0 @@
-minimap2 -x asm5 -t 1 \
--N 1000 \
--p 0.0   \
-synthetic_genome.fasta synthetic_genome.fasta > syn.self.asm5.paf
